ed7a6ceb0ae46cc0e91ac6c762b70bbea1e53f78
talk2dennis <adigwedennis@gmail.com>
2025-07-15 18:27:48 +0100

    moved back dev dependecies

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const url = NODE_ENV === 'production'
-  ? 'https://movieplane.onrender.com'
+  ? 'https://naijaedu.onrender.com'
   : `http://localhost:${PORT}`;
 
 
